61cf3bcd3c79f233ed612eac729b1702d03c75fb
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-06-26 17:23:37 +1000

    create functions to update score

--- script.js
+++ script.js
@@ -6,6 +6,38 @@ let guestPointsEl = document.getElementById("guest-score")
 let homeScore = 0
 let guestScore = 0
 
+// Function to update the score
+
+function addOnePointHome() {
+    homeScore += 1
+    homePointsEl.textContent = homeScore
+}
+
+function addTwoPointsHome() {
+    homeScore += 2
+    homePointsEl.textContent = homeScore
+}
+
+function addThreePointsHome() {
+    homeScore += 3
+    homePointsEl.textContent = homeScore
+}
+
+function addOnePointGuest() {
+    guestScore += 1
+    guestPointsEl.textContent = guestScore
+}
+
+function addTwoPointsGuest() {
+    guestScore += 2
+    guestPointsEl.textContent = guestScore
+}
+
+function addThreePointsGuest() {
+    guestScore += 3
+    guestPointsEl.textContent = guestScore
+}
+
 
 
 
