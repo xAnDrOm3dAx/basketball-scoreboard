433fc6874a9a63ee35369d67e55ba3ace3cfc860
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-06-26 17:23:03 +1000

    initialize scores

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 let homePointsEl = document.getElementById("home-score");
 let guestPointsEl = document.getElementById("guest-score")
 
+// Initialize scores
+let homeScore = 0
+let guestScore = 0
+
 
 
 
