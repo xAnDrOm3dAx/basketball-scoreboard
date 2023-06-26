e2d2593acd1c4caee34f18a49c9bdc1d4e6e75b3
xAnDrOm3dAx <123436349+xAnDrOm3dAx@users.noreply.github.com>
2023-06-26 17:22:45 +1000

    get required elements

---
+++ script.js
@@ -0,0 +1,10 @@
+// Get the required elements
+let homePointsEl = document.getElementById("home-score");
+let guestPointsEl = document.getElementById("guest-score")
+
+
+
+
+// // Initialize points
+// const points = [1, 2, 3]
+
